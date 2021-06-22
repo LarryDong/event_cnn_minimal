45ef0ff1660617bf3ecf74614e217fc293d6daf4
zhujun <zhujun@server.com>
2021-06-22 14:30:14 +0800

    可设置样本数以及修改config文件

--- my_own_tools/generate_path_file.py
+++ my_own_tools/generate_path_file.py
@@ -7,22 +7,33 @@ train.csv, valid.csv include all
 
 import os
 import pandas as pd
+import json
 
-train_path='/home/larrydong/dataset/e2vid/train'
-valid_path='/home/larrydong/dataset/e2vid/valid'
+data_path = '/home/zhujun/Documents/data/e2vid/'
+config_path = '/home/zhujun/Documents/lenovo_dvs/event_cnn_minimal/config/'
 
-L = []
-for root, dirs, files in os.walk(train_path):
-    for file in files:  # 遍历所有文件名
-        if os.path.splitext(file)[1] == '.h5':  
-            L.append(os.path.join(root, file))  # 拼接处绝对路径并放入列表
-df = pd.DataFrame.from_dict(L)
-df.to_csv('train.csv', header=None, index=None)
+max_num = 10
 
-L = []
-for root, dirs, files in os.walk(valid_path):
-    for file in files:  # 遍历所有文件名
-        if os.path.splitext(file)[1] == '.h5':  
-            L.append(os.path.join(root, file))  # 拼接处绝对路径并放入列表
-df = pd.DataFrame.from_dict(L)
-df.to_csv('valid.csv', header=None, index=None)
+# 生成csv
+train_path= data_path + 'train'
+valid_path= data_path + 'valid'
+for path in [train_path, valid_path]:
+    L = []
+    for root, dirs, files in os.walk(path):
+        for file in files:  # 遍历所有文件名
+            if os.path.splitext(file)[1] == '.h5':  
+                L.append(os.path.join(root, file))  # 拼接处绝对路径并放入列表
+            if len(L)>max_num: # 限制最大数量
+                break
+    df = pd.DataFrame.from_dict(L)
+    df.to_csv(path+'.csv', header=None, index=None)
+
+# 修改config
+name = 'reconstruction'
+with open(config_path+name+'.json','r') as f:
+        params = json.load(f)
+        params['data_loader']['args']['data_file'] = train_path + '.csv'
+        params['valid_data_loader']['args']['data_file'] = valid_path + '.csv'
+        params['trainer']['save_dir'] = data_path + 'model'
+with open(config_path+name+'.json','w') as f:
+        json.dump(params, f, indent=4)
